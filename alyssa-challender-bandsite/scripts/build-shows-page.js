54b06c6e78ed1ea0cad8d28c725f4c3df9d16102
Alyssa Challender <alysurrr@gmail.com>
2021-08-09 17:10:13 -0400

    display comments added

--- alyssa-challender-bandsite/scripts/build-shows-page.js
+++ alyssa-challender-bandsite/scripts/build-shows-page.js
@@ -1,5 +1,5 @@
 
-let shows = [
+const shows = [
     {
         date: 'Mon Sept 06 2021',
         venue: 'Ronald Lane',
@@ -34,40 +34,80 @@ let shows = [
 
 const showsList = document.querySelector('.shows-list');
 
-/* <section class="shows">
-    <div class="shows__label">Dates</div>
-    <div class="shows__info shows__info--bold">Mon Sept 06 2021</div>
-    <div class="shows__label">Venue</div>
-    <div class="shows__info">Ronald Lane</div>
-    <div class="shows__label">Location</div>
-    <div class="shows__info">San Francisco, CA</div>
-    
-    <div class="button shows__buy"><a href="/">Buy Tickets</a></div>
-</section> */
-
-const generateShowContent = (showData) => {
-    // create shows div
-    const showElement = document.createElement('div');
+const generateShowContent = (shows) => {
+    const showElement = document.createElement('article');
     showElement.classList.add('shows');
-    
-    //create shows label
-    const showLabel = document.createElement('div');
-    showElement.classList.add('shows__label');
-    showLabel.innerText = 'Dates';
 
-    
+    const dateElement = document.createElement('ul');
+    dateElement.classList.add('dates');
+    showElement.appendChild(dateElement);
 
-    
-    //create shows info
+    const dateLabelElement = document.createElement('p');
+    dateLabelElement.innerText = 'Dates';
+    dateElement.appendChild(dateLabelElement);
 
-    
-    //create shows info--bold
+    const venueElement = document.createElement('ul');
+    venueElement.classList.add('venue');
+    showElement.appendChild(venueElement);
 
-    
-    //create buy ticket button
- 
+    const venueLabelElement = document.createElement('p');
+    venueLabelElement.innerText = 'Venue';
+    venueElement.appendChild(venueLabelElement);
 
+    const locationElement = document.createElement('ul');
+    locationElement.classList.add('location');
+    showElement.appendChild(locationElement);
+
+    const locationLabelElement = document.createElement('p');
+    locationLabelElement.innerText = 'Location';
+    locationElement.appendChild(locationLabelElement);
+
+    const buyElement = document.createElement('div');
+    buyElement.classList.add('shows__buy');
+    buyElement.innerText = "Buy Tickets";
+    showElement.appendChild(buyElement);
+
+    const listDate = document.createElement('li');
+    listDate.classList.add('shows__date', 'dates')
+    listDate.innerText = shows.date;
+    dateElement.appendChild(listDate);
+    
+    const listVenue = document.createElement('li');
+    listVenue.classList.add('shows__info', 'venue')
+    listVenue.innerText = shows.venue;
+    venueElement.appendChild(listVenue);
+    
+    const listLocation = document.createElement('li');
+    listLocation.classList.add('shows__info', 'location')
+    listLocation.innerText = shows.location;
+    locationElement.appendChild(listLocation);
+        
+    
+    for(let i = 0; i < shows.length; i++) {        
+        const item = shows[i];
+        const showsData = shows;
+    
+    const showCard = generateShowContent(showsData);
+    
+    showsList.appendChild(showCard);
+      for (let key in item) {
+          console.log(item[key]);
+      }
+    }
     return showElement;
 }
+const generateCardList = (shows) => {
+    for (let i = 0; i < shows.length; i++) {
+      const showData = shows[i];
+      console.log('Data: ', showData);
+  
+      const showCard = generateShowContent(showData);
+      console.log('Card: ', showCard);
+  
+      showsList.appendChild(showCard);
+    }
+  }
 
-generateShowContent(shows);
+
+
+generateCardList(shows);
--- alyssa-challender-bandsite/scripts/index-page.js
+++ alyssa-challender-bandsite/scripts/index-page.js
@@ -1,14 +1,86 @@
+const display = [
+    {
+        name: "Connor Walton",
+        comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
+    },
+    {
+        name: "Emilie Beach",
+        comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
+    },
+    {
+        name: "Connor Walton",
+        comment: "I can't stop listening. Every time I hear one of their songs-the vocals-it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
+    }
+]
+
+const container = document.querySelector('.comments-display')
+
 const form = document.getElementById('form');
 const name = document.getElementById('name');
 const comment = document.getElementById('comment');
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
+const generateComments = (display) => {
+    const commentElement = document.createElement('article');
+    commentElement.classList.add('comments__input');
 
-    if(name.value === '' || name.value == null) {
-        name.style.borderColor="#D22D2D";
+    const nameElement = document.createElement('h3');
+    nameElement.classList.add('comments__name');
+    nameElement.innerText = display.name;
+    commentElement.appendChild(nameElement);
+
+    const textElement = document.createElement('p');
+    textElement.classList.add('comments__text');
+    textElement.innerText = display.comment;
+    commentElement.appendChild(textElement);
+
+    for(let i = 0; i < display.length; i++) {        
+        const item = display[i];
+        const commentsData = display;
+    
+        const commentCard = generateCommentContent(commentsData);
+    
+        container.appendChild(commentCard);
+        for (let key in item) {
+            console.log(item[key]);
+        }
     }
-    if(comment.value === '' || comment.value == null) {
-        comment.style.borderColor="#D22D2D";
+
+
+    return commentElement;
+
+}
+
+
+const generateCommentList = (display) => {
+    for (let i = 0; i < display.length; i++) {
+      const commentData = display[i];
+      console.log('Data: ', commentData);
+
+      const commentCard = generateComments(commentData);
+      console.log('Card: ', commentCard);
+  
+      container.appendChild(commentCard);
     }
-})
+  }
+
+
+
+generateCommentList(display);
+
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault();
+
+//     if(name.value === '' || name.value == null) {
+//         name.style.borderColor="#D22D2D";
+//     }
+//     if(comment.value === '' || comment.value == null) {
+//         comment.style.borderColor="#D22D2D";
+//     }
+// })
+
+
+// commentInfo.unshift(newComment);
+// commentList.innerHTML = '';
+// displayComment(commentInfo);
+
+// stopProp();
