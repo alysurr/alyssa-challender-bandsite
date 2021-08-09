bb4325bbfef036d981c1489c31496d15b1226709
Alyssa Challender <alysurrr@gmail.com>
2021-08-09 17:39:46 -0400

    added placeholder comments

--- alyssa-challender-bandsite/scripts/index-page.js
+++ alyssa-challender-bandsite/scripts/index-page.js
@@ -1,15 +1,18 @@
 const display = [
     {
         name: "Connor Walton",
-        comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
+        comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+        date: "02/17/2021"
     },
     {
         name: "Emilie Beach",
-        comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
+        comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+        date: "01/09/2021"
     },
     {
         name: "Connor Walton",
-        comment: "I can't stop listening. Every time I hear one of their songs-the vocals-it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
+        comment: "I can't stop listening. Every time I hear one of their songs-the vocals-it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+        date: "12/20/2020"
     }
 ]
 
@@ -23,10 +26,23 @@ const generateComments = (display) => {
     const commentElement = document.createElement('article');
     commentElement.classList.add('comments__input');
 
+    const commentAside = document.createElement('aside');
+    commentAside.classList.add('comments__image-placeholder');
+    container.appendChild(commentAside);
+
+    const flexElement = document.createElement('div');
+    flexElement.classList.add('comments__flex');
+    commentElement.appendChild(flexElement);
+
     const nameElement = document.createElement('h3');
     nameElement.classList.add('comments__name');
     nameElement.innerText = display.name;
-    commentElement.appendChild(nameElement);
+    flexElement.appendChild(nameElement);
+    
+    const dateElement = document.createElement('div');
+    dateElement.classList.add('comments__date');
+    dateElement.innerText = display.date;
+    flexElement.appendChild(dateElement);
 
     const textElement = document.createElement('p');
     textElement.classList.add('comments__text');
@@ -36,10 +52,10 @@ const generateComments = (display) => {
     for(let i = 0; i < display.length; i++) {        
         const item = display[i];
         const commentsData = display;
-    
         const commentCard = generateCommentContent(commentsData);
     
         container.appendChild(commentCard);
+
         for (let key in item) {
             console.log(item[key]);
         }
